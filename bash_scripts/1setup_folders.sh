94f058d3c176c0304a0580f4551d6dbb55df2d23
danhan52 <danny.hanson52@gmail.com>
2018-02-20 13:16:53 -0600

    fix the first script

--- bash_scripts/1setup_folders.sh
+++ bash_scripts/1setup_folders.sh
@@ -5,6 +5,7 @@ mkdir ../data/iam
 mkdir ../data/combined
 
 # create modeling folders
+mkdir ../modeling/tf_output
 mkdir ../modeling/tf_output/estimator
 mkdir ../modeling/tf_output/graph
 mkdir ../modeling/tf_output/prediction
