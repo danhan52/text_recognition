978770f193dedb4210e62226b772a8ae0da5351e
danhan52 <danny.hanson52@gmail.com>
2018-04-28 23:25:21 -0500

    One more fix for file size experiment

--- modeling/train_and_validate_bentham.sh
+++ modeling/train_and_validate_bentham.sh
@@ -1,15 +1,15 @@
 # train model for one epoch
-python run_model.py train BenthamDataset 1 16 False 0 $1 new
+python run_model.py train BenthamDataset 1 16 True 0 $1 new
 # prediction
-python run_model.py pred BenthamDataset 1 16 False 0 $1 new $1 0
+python run_model.py pred BenthamDataset 1 16 True 0 $1 new $1 0
 python end_batch.py 0
 
 for i in {1000..4000..1000}
   do
 	# train model for one epoch
-	python run_model.py train BenthamDataset 1 16 False $i $1 new $1 $(($i-1000))
+	python run_model.py train BenthamDataset 1 16 True $i $1 new $1 $(($i-1000))
 	# prediction
-	python run_model.py pred BenthamTest 1 16 False $i $1 new $1 $i
+	python run_model.py pred BenthamTest 1 16 True $i $1 new $1 $i
 
 	# delete old files
 	python end_batch.py $i $1 2000
