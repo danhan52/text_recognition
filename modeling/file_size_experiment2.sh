cd1707598652a09f12051ec6b49bbad3dedb7711
danhan52 <danny.hanson52@gmail.com>
2018-04-30 07:35:08 -0500

    Trying to see if iam is better

--- modeling/file_size_experiment2.sh
+++ modeling/file_size_experiment2.sh
@@ -2,34 +2,34 @@ mkdir -p ./tf_output/file_size
 
 # do small images
 cd ../preprocess
-python preprocess_both_bentham.py 0.25
+python preprocess_both_iam.py 0.25
 
 cd ../modeling
 mkdir -p ./tf_output/file_size/size25/
 # training
-python run_model.py train BenthamDataset 5 16 True 0 ./tf_output/file_size/size25/ new
+python run_model.py train iamHandwriting 5 16 True 0 ./tf_output/file_size/size25/ new
 # prediction
-python run_model.py pred BenthamTest 1 16 True 1000 ./tf_output/file_size/size25/ new ./tf_output/file_size/size25/ 0
+python run_model.py pred iamTest 1 16 True 1000 ./tf_output/file_size/size25/ new ./tf_output/file_size/size25/ 0
 
 
 # do medium images
 cd ../preprocess
-python preprocess_both_bentham.py 0.5
+python preprocess_both_iam.py 0.5
 
 cd ../modeling
 mkdir -p ./tf_output/file_size/size50/
 # training
-python run_model.py train BenthamDataset 5 16 True 0 ./tf_output/file_size/size50/ new
+python run_model.py train iamHandwriting 5 16 True 0 ./tf_output/file_size/size50/ new
 # prediction
-python run_model.py pred BenthamTest 1 16 True 1000 ./tf_output/file_size/size50/ new ./tf_output/file_size/size50/ 0
+python run_model.py pred iamTest 1 16 True 1000 ./tf_output/file_size/size50/ new ./tf_output/file_size/size50/ 0
 
 # do full size images
 cd ../preprocess
-python preprocess_both_bentham.py 1.0
+python preprocess_both_iam.py 1.0
 
 cd ../modeling
 mkdir -p ./tf_output/file_size/size100/
 # training
-python run_model.py train BenthamDataset 5 16 True 0 ./tf_output/file_size/size100/ new
+python run_model.py train iamHandwriting 5 16 True 0 ./tf_output/file_size/size100/ new
 # prediction
-python run_model.py pred BenthamTest 1 16 True 1000 ./tf_output/file_size/size100/ new ./tf_output/file_size/size100/ 0
+python run_model.py pred iamTest 1 16 True 1000 ./tf_output/file_size/size100/ new ./tf_output/file_size/size100/ 0
