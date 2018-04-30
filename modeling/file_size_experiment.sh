c10c895e998da194ca8bf7a6a62d7811912a2241
danhan52 <danny.hanson52@gmail.com>
2018-04-30 07:37:37 -0500

    File size exp again

--- modeling/file_size_experiment.sh
+++ modeling/file_size_experiment.sh
@@ -2,24 +2,24 @@ mkdir -p ./tf_output/file_size
 
 # do small images
 cd ../preprocess
-python preprocess_both_bentham.py 0.25
+python preprocess_both_iam.py 0.25
 
 cd ../modeling
 mkdir -p ./tf_output/file_size/size25/
-bash train_and_validate_bentham.sh ./tf_output/file_size/size25/
+bash train_and_validate_iam.sh ./tf_output/file_size/size25/
 
 # do medium images
 cd ../preprocess
-python preprocess_both_bentham.py 0.5
+python preprocess_both_iam.py 0.5
 
 cd ../modeling
 mkdir -p ./tf_output/file_size/size50/
-bash train_and_validate_bentham.sh ./tf_output/file_size/size50/
+bash train_and_validate_iam.sh ./tf_output/file_size/size50/
 
 # do full size images
 cd ../preprocess
-python preprocess_both_bentham.py 1.0
+python preprocess_both_iam.py 1.0
 
 cd ../modeling
 mkdir -p ./tf_output/file_size/size100/
-bash train_and_validate_bentham.sh ./tf_output/file_size/size100/
+bash train_and_validate_iam.sh ./tf_output/file_size/size100/
